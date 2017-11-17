063db34890cd0bf6a097ff94179fb34be5966881
marquos <marcos.martval@gmail.com>
2017-11-17 20:52:01 +0100

    done sin explosiones

---
+++ 3d1/3d1/Assets/scripts/BallController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	private int isJumping;
+	private int numMaxSaltos;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = GetComponent<Rigidbody>();
+		this.speed = 15;
+	}
+	
+
+	void FixedUpdate () {
+		float horizontalAxis = Input.GetAxis("Horizontal");
+		float verticalAxis = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(horizontalAxis, 0.0f,verticalAxis);
+		rb.AddForce(movement*speed);
+
+		if (Input.GetKeyDown ("space") && numMaxSaltos > 0) {
+			Vector3 jumpForce = new Vector3 (0.0f, 3.0f, 0.0f);
+			rb.AddForce (jumpForce, ForceMode.Impulse);
+			numMaxSaltos--;
+		}
+	} 
+	void OnCollisionEnter (Collision c){
+		this.numMaxSaltos = 2;
+	}
+}
