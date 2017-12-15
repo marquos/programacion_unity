c561b1637f0e5419df3ebac56cbe8a50f3012fa7
marquos <marcos.martval@gmail.com>
2017-12-15 19:52:36 +0100

    torreta

---
+++ Assets/shooterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooterController : MonoBehaviour {
+
+	public GameObject shootable;
+	private GameObject player;
+	private Transform t;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+		t = shootable.transform;
+		StartCoroutine (Fire());
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.LookAt (player.transform.position);
+	}
+
+	private IEnumerator Fire(){
+
+		while (true) {
+			
+			GameObject shoot = (GameObject)Instantiate (shootable, t.position, t.rotation);
+			shoot.SetActive (true);
+			shoot.transform.LookAt (player.transform.position);
+			Vector3 diffForce = player.transform.position - shoot.transform.position;
+			shoot.GetComponent <Rigidbody> ().velocity = diffForce * 3;
+
+			Destroy (shoot, 1);
+			yield return new WaitForSeconds (2.0f);
+		}
+	}
+}
